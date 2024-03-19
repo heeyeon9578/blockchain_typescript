3b9c453a646058af19051319a00c56f0d6c5a7cd
최희연 <heeyeon95781@gmail.com>
2024-03-19 20:13:07 +0900

    no message

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@
 //init()
 
 //console.log("bye");
+import * as crypto from  "crypto";
 interface BlockShape{
     hash: string;
     prevHash: string;
@@ -24,5 +25,11 @@ class Block implements BlockShape{
         public prevHash: string,
         public height: number,
         public data:string,
-    ){}
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash: string, height: number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+
+    }
 }
